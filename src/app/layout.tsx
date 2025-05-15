d96a9653f904e72d5deb4fe9e73869737427b3fe
paulorn <paulonormie@gmail.com>
2025-05-15 14:17:41 -0300

    Task02 | feat | adicionando descrição do site na metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,13 @@ import Footer from '@/components/Footer/Footer'
 
 export const metadata = {
     title: 'Alexandre Cabanel',
+    description: 'Explore the life and works of Alexandre Cabanel, a 19th-century French academic painter.',
 }
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>
+        <body>
                 <div className="layout">
                     <Header/>
                     <NavBar/>
