e4ba6cba0e469b3f31be429603ae9378a24af0ce
paulorn <paulonormie@gmail.com>
2025-05-15 13:39:04 -0300

    Task02 | fix | corrigindo apóstrofo

--- src/app/highlights/page.tsx
+++ src/app/highlights/page.tsx
@@ -4,13 +4,13 @@ export default function HighlightsPage() {
             <h1>Highlighted Works</h1>
             <ul>
                 <li>
-                    <strong>The Birth of Venus (1863):</strong> Cabanel's most iconic painting, representing the goddess emerging from the sea with a dreamlike and sensual elegance.
+                    <strong>The Birth of Venus (1863):</strong> Cabanel&#39;s most iconic painting, representing the goddess emerging from the sea with a dreamlike and sensual elegance.
                 </li>
                 <li>
                     <strong>Fallen Angel (1847):</strong> A melancholic and emotional portrait of a weeping angel, admired for its expressiveness and technical mastery.
                 </li>
                 <li>
-                    <strong>Albaydé (1848):</strong> An early work inspired by Victor Hugo's “Les Orientales,” showcasing Cabanel’s fascination with exotic beauty and orientalism.
+                    <strong>Albaydé (1848):</strong> An early work inspired by Victor Hugo&#39;s “Les Orientales,” showcasing Cabanel’s fascination with exotic beauty and orientalism.
                 </li>
             </ul>
             <p>
